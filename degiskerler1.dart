0b43e2e865b0038c99ae5e68027628c110f0a900
Youtube <80195150+checkupcodes@users.noreply.github.com>
2022-02-09 17:09:54 +0300

    v2
    değişkenler1 videosu kaynak kodları

---
+++ degiskerler1.dart
@@ -0,0 +1,38 @@
+import 'dart:typed_data';
+
+void main() {
+  var cekabinYasi = 21;
+  var cekabinBoyu = 1.85;
+  var cekapSohbet = "Kanalıma \n HoşGeldin'iz"
+      "khlkhnl jhkl jhk kjhkjh kh bkjbh \n ]bkhb kjbk bkhb kb hmnm bk"
+      "akwnlkanwlkfnalwknfl";
+
+  var cekapKarar = true;
+
+  List<String> ogrenciPuanlar = ["90", "32", "65", "selamlar"];
+
+  var AllahNeVerdiyse = [bool, "90", 32 , 42.2];
+
+
+
+  //MAP
+  Map<String, String> evliInsan = {
+    "Baskan" : "Cekap",
+    "Yardımcı" : "Adem"
+  };
+
+  Map<int , String> kura = {
+    1 : "araba",
+    2 : "sakiz",
+    3 : "bisiklet",
+  };
+
+  var AllahNeVerdiyseMap = {
+    1 : "yakup",
+    3.21 : true,
+    "Yusuf " : 4.21,
+  };
+
+  print(AllahNeVerdiyseMap);
+
+}
